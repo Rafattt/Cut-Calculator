c6900199ed5db70f0a6e7786c5f009befdef77bf
Rafattt <rafatpl1@gmail.com>
2018-04-24 15:55:10 -0500

    fixed bugs
    fixed bug with RO without "-" and with RO = (x*12)-2

--- src/js/convertToFeets.js
+++ src/js/convertToFeets.js
@@ -1,11 +1,26 @@
 const convertToFeets = (data) => {
     console.log(data);
 
-    if((variables.roughSize()-2) % 12 ===0){// checks if data is integer
+    let splitedNumber = data.split("-"); //splitting rough opening text data into feets and inches
+    let firstNumber = variables.roughSize().split("-");//getting first number from RO field
+    firstNumber = firstNumber[0];
+
+    if((firstNumber-2) % 12 === 0 && (variables.roughSize()-2) % 12 !==0){
+        console.log(data);
+        let splitedNumber = data.split("-"); //splitting rough opening text data into feets and inches
+        console.log(splitedNumber[0] + " "+splitedNumber[1]);
+        let splitedNumberInch = splitedNumber[1].split("/");
+        let feetInchArray = [splitedNumber[0],"",
+        splitedNumberInch[0],splitedNumberInch[1]/12];
+        return feetInchArray;
+    }else if((variables.roughSize()-2) % 12 ===0){// checks if data is integer
         let size = parseFloat(variables.roughSize())-2;
         return size;
     } else if((variables.roughSize()-2) % 12 !==0 &&variables.roughSize().indexOf("-")>0){
-        let splitedNumber = data.split("-"); //splitting rough opening text data into feets and inches
+       
+       
+
+        
         splitedNumberInch = splitedNumber[1].split("/");
         splitedNumberInch[0] = parseFloat(splitedNumberInch[0]);
         splitedNumberInch[1] = parseFloat(splitedNumberInch[1])/12;
@@ -13,6 +28,7 @@ const convertToFeets = (data) => {
         splitedNumber[1] = calculateInches(splitedNumberInch[0],splitedNumberInch[1]);
         let splitedNumberOneSplit = splitedNumber[1].split("-");
         splitedNumber[1] = splitedNumberOneSplit[0];
+        console.log("splitedNumber[0] "+splitedNumber[0]);
         let lastNumbersSmall = splitedNumberOneSplit[1].split("/");
         splitedNumber[0] = splitedNumber[0]+"'";
         let smallNumbersText = lastNumbersSmall[0]+"/"+lastNumbersSmall[1]+'"';
